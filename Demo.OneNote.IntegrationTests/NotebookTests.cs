927e19692a33cbbdc11d1f930bf1d00e2c5ef16a
Egor Bukharov <egabrain@mail.ru>
2018-10-03 23:24:39 +0700

    Added integration tests

---
+++ Demo.OneNote.IntegrationTests/NotebookTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.OneNote.IntegrationTests
+{
+    [TestClass]
+    public class NotebookTests
+    {
+        [TestMethod]
+        public void OpenedNotebookShouldNotBeNull()
+        {
+            const string relativePathToInputFile = @"TestInputFiles\OneNoteApi Demo.one";
+            var notebook = Notebook.Open(relativePathToInputFile);
+
+            Assert.IsNotNull(notebook);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ShouldThrowAnExceptionIfFIleNotExists()
+        {
+            const string relativePathToInputFile = @"TestInputFiles\Not existing file.one";
+            Notebook.Open(relativePathToInputFile);
+        }
+    }
+}
---
+++ Demo.OneNote/Notebook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Demo.OneNote
+{
+    public class Notebook
+    {
+        protected Notebook() { }
+
+        public static Notebook Open(string path)
+        {
+            using (var stream = File.OpenRead(path))
+            {
+                return new Notebook();
+            }
+        }
+    }
+}
